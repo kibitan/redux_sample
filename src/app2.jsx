952651eda409a5b3cfb35dee21dd7a2d17f2ea19
kibitan <uzukifirst@gmail.com>
2016-09-20 11:39:50 +0900

    redux-react のコードサンプルを追加

--- src/app2.jsx
+++ src/app2.jsx
@@ -1,7 +1,10 @@
 // Reduxの実装とReactとの連携を超シンプルなサンプルを使って解説 | mae's blog
 // http://mae.chab.in/archives/2885
 
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
 /* Storeの実装 */
 
@@ -38,3 +41,79 @@ function formReducer(state, action) {
       return state;
   }
 }
+
+// Veiw (Container Components)
+class FormApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <FormInput handleClick={this.props.onClick} />
+        <FormDisplay data={this.props.value} />
+      </div>
+    );
+  }
+}
+FormApp.propTypes = {
+  onClick: React.PropTypes.func.isRequired,
+  value: React.PropTypes.string,
+};
+
+
+// Veiw (Presentational Components)
+class FormInput extends React.Component {
+  send(e) {
+    e.preventDefault();
+    this.props.handleClick(this.myInput.value.trim());
+    this.myInput.value = '';
+    return;
+  }
+  render() {
+    return (
+      <form>
+        <input type="text" ref={(ref) => (this.myInput = ref)} defaultValue="" />
+        <button onClick={(event) => this.send(event)}>Send</button>
+      </form>
+    );
+  }
+}
+FormInput.propTypes = {
+  handleClick: React.PropTypes.func.isRequired,
+};
+
+// Veiw (Presentational Components)
+class FormDisplay extends React.Component {
+  render() {
+    return (
+      <div>{this.props.data}</div>
+    );
+  }
+}
+FormDisplay.propTypes = {
+  data: React.PropTypes.string,
+};
+
+// Connect to Redux
+function mapStateToProps(state) {
+  return {
+    value: state.value,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick(value) {
+      dispatch(send(value));
+    },
+  };
+}
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FormApp);
+
+// Rendering
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.querySelector('.content')
+);
